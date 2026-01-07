302b2cb00935183286e5224d18de743bb7e43f7d
sgtqwq <2895309236@qq.com>
2026-01-07 22:34:01 +0800

    Do not reduce depth when opponent's position worsens
    Do not reduce depth when opponent's position worsens

--- src/search.cpp
+++ src/search.cpp
@@ -753,7 +753,7 @@ Value Search::Worker::search(
     // Hindsight adjustment of reductions based on static evaluation difference.
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
+    if (priorReduction >= 2 && depth >= 2 && !opponentWorsening && ss->staticEval + (ss - 1)->staticEval > 168)
         depth--;
 
     // At non-PV nodes we check for an early TT cutoff
